a77989173090ca1faf1ef1e546a01255927bf527
Christian Zobel <zobel.christian@gmail.com>
2018-07-22 09:19:11 +0200

    added basic angular 6 UI

---
+++ profile-frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
